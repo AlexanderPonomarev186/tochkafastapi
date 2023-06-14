b556a9a6f4c73a8b05a5fd31e70f5ccd6f500197
Александр Пономарев <113848436+AlexanderPonomarev186@users.noreply.github.com>
2023-06-14 12:51:21 +0500

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:u2fRzYDFXIJPAtHnvZa0@containers-us-west-143.railway.app:6734/railway"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:f8zPHzRrXF5M1oaf4b7y@containers-us-west-13.railway.app:6707/railway"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
